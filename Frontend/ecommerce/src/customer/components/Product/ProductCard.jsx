3de38c3295103e248f9a0bd1615a737890246371
NeerajGupta23 <230803neeraj@gmail.com>
2024-08-25 11:39:36 +0530

    Filter Module and params is Done and working fine

--- Frontend/ecommerce/src/customer/components/Product/ProductCard.jsx
+++ Frontend/ecommerce/src/customer/components/Product/ProductCard.jsx
@@ -1,27 +1,29 @@
 import React from "react";
 import "./ProductCard.css";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
   return (
     <div className="productCard w-[15rem] m-3 transition-all cursor-pointer">
       <div className="h-[20rem]">
         <img
           className=" h-full w-full object-cover object-left-top"
-          src="https://rukminim1.flixcart.com/image/612/612/l01blow0/top/m/q/z/m-gr6245-white-harpa-original-imagbwnh3xqhgfbn.jpeg?q=70"
+          src={product.imageUrl}
           alt=""
         />
       </div>
-      <div className="textPart bg-white p-3 ">
+
+      <div className="textPart text-left bg-white p-3 ">
         <div>
-          <p className="font-bold opacity-60 float-left">HARPA</p>
-          <p className="float-left text-justify">
-            Casual Puff Sleeves Solid Women White Top
-          </p>
+          <p className="font-bold opacity-60">{product.brand}</p>
+          <p>{product.title}</p>
         </div>
-        <div className="flex items-center space-x-2 float-left">
-          <p className="font-semibold">₹1399</p>
-          <p className="line-through opacity-50">₹2399</p>
-          <p className="text-green-600 font-semibold">50% off</p>
+
+        <div className="flex items-center space-x-2">
+          <p className="font-semibold">₹{product.discountedPrice}</p>
+          <p className="line-through opacity-50">₹{product.price}</p>
+          <p className="text-green-600 font-semibold">
+            {product.discountPersent}% off
+          </p>
         </div>
       </div>
     </div>
